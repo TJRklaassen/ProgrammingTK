65fc3ae21e160b1e77bfe856cf01f435febe4cb2
TJRklaassen <tjrklaassen@hotmail.com>
2017-09-08 12:09:05 +0200

    Les 2 opdrachten voltooid

---
+++ Les2/pe2_2.py
@@ -0,0 +1,8 @@
+cijferICOR = 5.5
+cijferPROG = 8
+cijferCSN = 7
+
+gemiddelde = (cijferICOR+cijferPROG+cijferCSN)/3
+beloning = (cijferICOR+cijferPROG+cijferCSN)*30
+overzicht = 'Mijn cijfers (gemiddeld een '+str(gemiddelde)+') leveren een beloning van € '+str(beloning)+' op!'
+print(overzicht)
---
+++ Les2/pe2_4.py
@@ -0,0 +1,4 @@
+uurloon = float(input('Wat verdien je per uur:'))
+aantalUur = int(input('Hoeveel uur heb je gewerkt:'))
+print(str(aantalUur)+' uur werken levert '+str(aantalUur*uurloon)+' euro op.')
+x = hi
